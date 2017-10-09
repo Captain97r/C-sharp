8ae12ddce7f94168c3c16a85fe2bfb030a060ab6
Михаил Лямаев <misha25_live@mail.ru>
2017-10-10 09:37:18 +1000

    It is drowing, but dont save points

---
+++ Redactor_Vector_Graph/Redactor_Vector_Graph/Main_Drow_Form.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Redactor_Vector_Graph
+{
+    partial class Main_Drow_Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PaintBox = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PaintBox
+            // 
+            this.PaintBox.BackColor = System.Drawing.SystemColors.Window;
+            this.PaintBox.Location = new System.Drawing.Point(278, 12);
+            this.PaintBox.Name = "PaintBox";
+            this.PaintBox.Size = new System.Drawing.Size(680, 464);
+            this.PaintBox.TabIndex = 0;
+            this.PaintBox.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            this.PaintBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PaintBox_MouseDown);
+            this.PaintBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PaintBox_MouseMove);
+            this.PaintBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PaintBox_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 85);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 488);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PaintBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PaintBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Redactor_Vector_Graph/Redactor_Vector_Graph/Main_Drow_Form.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Redactor_Vector_Graph
+{
+    public partial class Main_Drow_Form : Form
+    {
+
+        bool Flag_Right_CLick = false;
+        Graphics graph_PaintBox;
+        Point lastPoint = new Point(0,0);
+        Pen pen1 = new Pen(Color.Blue);
+        public Main_Drow_Form()
+        {
+            InitializeComponent();
+            graph_PaintBox = PaintBox.CreateGraphics();
+            this.Paint += Form1_Paint;
+        
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            graph_PaintBox.DrawLine(pen1, 0, 0, 120, 120);
+
+        
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            graph_PaintBox.Clear(Color.White);
+        }
+
+        private void PaintBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+              
+                Flag_Right_CLick = true;
+                lastPoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void PaintBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+               Flag_Right_CLick = false;
+
+            }
+        }
+
+        private void PaintBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Flag_Right_CLick)
+            {
+                graph_PaintBox.DrawLine(pen1, lastPoint, new Point(e.X,e.Y));
+                lastPoint = new Point(e.X, e.Y);
+            }
+        }
+    }
+}
