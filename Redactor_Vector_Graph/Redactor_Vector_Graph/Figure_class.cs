0f629330057e0cd3473f5d135e42710455711e82
Михаил Лямаев <misha25_live@mail.ru>
2017-11-23 23:12:06 +1000

    Add SelectArea in Ellipse, Line, Rect, RoundedRect

--- Redactor_Vector_Graph/Redactor_Vector_Graph/Figure_class.cs
+++ Redactor_Vector_Graph/Redactor_Vector_Graph/Figure_class.cs
@@ -158,6 +158,16 @@ namespace Redactor_Vector_Graph
             isSelected = false;
             return false;
         }
+        public override bool SelectArea(Rectangle area)
+        {
+            if (area.Contains(startPointW.ToScrPnt()) && area.Contains(endPointW.ToScrPnt()))
+            {
+                isSelected = true;
+                return true;
+            }
+            isSelected = false;
+            return false;
+        }
         public override void AddPoint(PointW pointW)
         {
             endPointW = pointW;
@@ -186,13 +196,13 @@ namespace Redactor_Vector_Graph
     {
         Rectangle rectColider;
         PointW startPointW;
-        PointW lastPointW;
+        PointW endPointW;
         int radius;
         public RoundedRect(Pen setPen, PointW start, int setRadius, Color? setColorFill = null)
         {
             pen = (Pen)setPen.Clone();
             startPointW = start;
-            lastPointW = start;
+            endPointW = start;
             radius = setRadius;
             if (setColorFill.HasValue)
             {
@@ -211,18 +221,28 @@ namespace Redactor_Vector_Graph
             isSelected = false;
             return false;
         }
+        public override bool SelectArea(Rectangle area)
+        {
+            if (area.Contains(startPointW.ToScrPnt()) && area.Contains(endPointW.ToScrPnt()))
+            {
+                isSelected = true;
+                return true;
+            }
+            isSelected = false;
+            return false;
+        }
         public override void AddPoint(PointW pointW)
         {
-            lastPointW = pointW;
+            endPointW = pointW;
 
         }
         public override void Draw(Graphics graphics)
         {
             int x, y;
-            x = Math.Min(startPointW.ToScrPnt().X, lastPointW.ToScrPnt().X);
-            y = Math.Min(startPointW.ToScrPnt().Y, lastPointW.ToScrPnt().Y);
-            int width = Math.Abs(startPointW.ToScrPnt().X - lastPointW.ToScrPnt().X);
-            int height = Math.Abs(startPointW.ToScrPnt().Y - lastPointW.ToScrPnt().Y);
+            x = Math.Min(startPointW.ToScrPnt().X, endPointW.ToScrPnt().X);
+            y = Math.Min(startPointW.ToScrPnt().Y, endPointW.ToScrPnt().Y);
+            int width = Math.Abs(startPointW.ToScrPnt().X - endPointW.ToScrPnt().X);
+            int height = Math.Abs(startPointW.ToScrPnt().Y - endPointW.ToScrPnt().Y);
             rectColider = new Rectangle(x - (int)Math.Round(pen.Width / 2, MidpointRounding.AwayFromZero), y - (int)Math.Round(pen.Width / 2, MidpointRounding.AwayFromZero),
                         width + (int)(pen.Width), height + (int)(pen.Width));
 
@@ -270,7 +290,7 @@ namespace Redactor_Vector_Graph
     public class Line : Figure
     {
         PointW startPointW;
-        PointW lastPointW;
+        PointW endPointW;
         public Line(Pen setPen, PointW start)
         {
             pen = (Pen)setPen.Clone();
@@ -279,15 +299,25 @@ namespace Redactor_Vector_Graph
         public override bool SelectPoint(Point pntClick)
         {
             const int dist = 20;
-            double A, B, C, distToline, distToLastPoint, distToSatrtPoint, lineLength;
-            A = startPointW.ToScrPnt().Y - lastPointW.ToScrPnt().Y;
-            B = lastPointW.ToScrPnt().X - startPointW.ToScrPnt().X;
+            double A, B, C, distToline, distToEndPoint, distToSatrtPoint, lineLength;
+            A = startPointW.ToScrPnt().Y - endPointW.ToScrPnt().Y;
+            B = endPointW.ToScrPnt().X - startPointW.ToScrPnt().X;
             C = -(startPointW.ToScrPnt().X * A + startPointW.ToScrPnt().Y * B);
-            lineLength = Math.Sqrt(Math.Pow(startPointW.ToScrPnt().X - lastPointW.ToScrPnt().X, 2.0) + Math.Pow(startPointW.ToScrPnt().Y - lastPointW.ToScrPnt().Y, 2.0));
+            lineLength = Math.Sqrt(Math.Pow(startPointW.ToScrPnt().X - endPointW.ToScrPnt().X, 2.0) + Math.Pow(startPointW.ToScrPnt().Y - endPointW.ToScrPnt().Y, 2.0));
             distToline = (Math.Abs(A * pntClick.X + B * pntClick.Y + C) / (Math.Sqrt(A * A + B * B)));
             distToSatrtPoint = Math.Sqrt(Math.Pow(startPointW.ToScrPnt().X - pntClick.X, 2.0) + Math.Pow(startPointW.ToScrPnt().Y - pntClick.Y, 2.0)) - lineLength;
-            distToLastPoint = Math.Sqrt(Math.Pow(lastPointW.ToScrPnt().X - pntClick.X, 2.0) + Math.Pow(lastPointW.ToScrPnt().Y - pntClick.Y, 2.0)) - lineLength;
-            if (distToline <= dist && distToSatrtPoint < dist && distToLastPoint < dist)
+            distToEndPoint = Math.Sqrt(Math.Pow(endPointW.ToScrPnt().X - pntClick.X, 2.0) + Math.Pow(endPointW.ToScrPnt().Y - pntClick.Y, 2.0)) - lineLength;
+            if (distToline <= dist && distToSatrtPoint < dist && distToEndPoint < dist)
+            {
+                isSelected = true;
+                return true;
+            }
+            isSelected = false;
+            return false;
+        }
+        public override bool SelectArea(Rectangle area)
+        {
+            if (area.Contains(startPointW.ToScrPnt()) && area.Contains(endPointW.ToScrPnt()))
             {
                 isSelected = true;
                 return true;
@@ -297,17 +327,17 @@ namespace Redactor_Vector_Graph
         }
         public override void AddPoint(PointW pointW)
         {
-            lastPointW = pointW;
+            endPointW = pointW;
         }
         public override void Draw(Graphics graphics)
         {
-            graphics.DrawLine(pen, startPointW.ToScrPnt(), lastPointW.ToScrPnt());
+            graphics.DrawLine(pen, startPointW.ToScrPnt(), endPointW.ToScrPnt());
         }
         public override void DrawColider(Graphics graphics)
         {
             if (isSelected)
                 DrawColiderRect(graphics, new Rectangle(startPointW.ToScrPnt(),
-                    new Size(lastPointW.ToScrPnt().X - startPointW.ToScrPnt().X, lastPointW.ToScrPnt().Y - startPointW.ToScrPnt().Y)));
+                    new Size(endPointW.ToScrPnt().X - startPointW.ToScrPnt().X, endPointW.ToScrPnt().Y - startPointW.ToScrPnt().Y)));
         }
 
     }
@@ -315,12 +345,12 @@ namespace Redactor_Vector_Graph
     {
         Rectangle rectColider;
         PointW startPointW;
-        PointW lastPointW;
+        PointW endPointW;
         public Ellipse(Pen setPen, PointW start, Color? setColorFill = null)
         {
             pen = (Pen)setPen.Clone();
             startPointW = start;
-            lastPointW = start;
+            endPointW = start;
             if (setColorFill.HasValue)
             {
                 colorFill = (Color)setColorFill;
@@ -329,9 +359,19 @@ namespace Redactor_Vector_Graph
         }
         public override bool SelectPoint(Point pntClick)
         {
-            double A = (lastPointW.ToScrPnt().X - startPointW.ToScrPnt().X) / 2;
-            double B = (lastPointW.ToScrPnt().Y - startPointW.ToScrPnt().Y) / 2;
+            double A = (endPointW.ToScrPnt().X - startPointW.ToScrPnt().X) / 2;
+            double B = (endPointW.ToScrPnt().Y - startPointW.ToScrPnt().Y) / 2;
             if (((Math.Pow((pntClick.X - startPointW.ToScrPnt().X - A) / A, 2) + (Math.Pow((pntClick.Y - startPointW.ToScrPnt().Y - B) / B, 2))) <= 1))
+            {
+                isSelected = true;
+                return true;
+            }
+            isSelected = false;
+            return false;
+        }
+        public override bool SelectArea(Rectangle area)
+        {
+            if (area.Contains(startPointW.ToScrPnt()) && area.Contains(endPointW.ToScrPnt()))
             {
                 isSelected = true;
                 return true;
@@ -341,12 +381,12 @@ namespace Redactor_Vector_Graph
         }
         public override void AddPoint(PointW pointW)
         {
-            lastPointW = pointW;
+            endPointW = pointW;
         }
         public override void Draw(Graphics graphics)
         {
             rectColider = new Rectangle(startPointW.ToScrPnt(),
-                    new Size(lastPointW.ToScrPnt().X - startPointW.ToScrPnt().X, lastPointW.ToScrPnt().Y - startPointW.ToScrPnt().Y));
+                    new Size(endPointW.ToScrPnt().X - startPointW.ToScrPnt().X, endPointW.ToScrPnt().Y - startPointW.ToScrPnt().Y));
             graphics.DrawEllipse(pen, rectColider);
             if (isFill)
                 graphics.FillEllipse(new SolidBrush(colorFill), rectColider);
